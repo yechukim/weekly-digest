89b10bb096aee003b49b720a9abcb434f8d8ed6b
yechu <yechukim217@gmail.com>
2022-10-08 22:26:30 +0900

    init

---
+++ pages/[id].tsx
@@ -0,0 +1,15 @@
+export async function getStaticPaths() {
+	const path = '' // id 가져오기
+	return {}
+}
+
+export async function getStaticProps() {
+	// get data
+	return {}
+}
+
+function Post() {
+	return <div>post</div>
+}
+
+export default Post
