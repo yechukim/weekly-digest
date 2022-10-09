6341d8cc1adf293487755284b990e303f93c9ec1
yechu <yechukim217@gmail.com>
2022-10-10 02:00:10 +0900

    feat: add types

---
+++ types/index.ts
@@ -0,0 +1,18 @@
+export type PostListType = {
+	id: string
+	data: {
+		[key: string]: any
+	}
+}
+
+export type PostType = {
+	id: string
+	data: {
+		[key: string]: any
+	}
+	content: string
+}
+
+export type DetailType = {
+	id: string
+}
